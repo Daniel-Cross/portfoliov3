e6ece4a98cc9dd4b3d34c7159bdf10a838f1a1a4
Daniel Cross <dan.s.cross@icloud.com>
2020-08-20 09:21:40 +0100

    Create jobData.js

---
+++ src/constants/jobData.js
@@ -0,0 +1,68 @@
+import intouchMobile from "../assets/intouchMobile.png";
+import intouchDesktop from "../assets/intouchDesktop.png";
+import skiddleMobile from "../assets/skiddleMobile.png";
+import skiddleDesktop from "../assets/skiddleDesktop.png";
+import bookingMobile from "../assets/bookingMobile.png";
+import bookingDesktop from "../assets/bookingDesktop.png";
+import degreeMobile from "../assets/degreeMobile.png";
+import degreeDesktop from "../assets/degreeDesktop.png";
+import keggyMobile from "../assets/keggyMobile.png";
+import keggyDesktop from "../assets/keggyDesktop.png";
+import codesMobile from "../assets/codesMobile.png";
+import codesDesktop from "../assets/codesDesktop.png";
+
+export const jobs = [
+  {
+    index: "1",
+    workplace: "Skiddle",
+    imgMobile: skiddleMobile,
+    imgDesktop: skiddleDesktop,
+    summary:
+      "Responsible for the development of the new promoters portal at the App Stores most downloaded ticketing service. A tired looking web portal, built previously using PHP, required modernising and new functionality. The application was rebuilt from the ground up using the latest version of React, with Hooks, styled-components and Jest.",
+  },
+
+  {
+    index: "2",
+    workplace: "InTouch Networks",
+    imgMobile: intouchMobile,
+    imgDesktop: intouchDesktop,
+    summary:
+      "During my time at InTouch Networks we completed a huge overhaul of the members portal from a tired, dated look to a more modern and sleek looking website with added functionality such as podcast playback, a forum, blog integration with a headless CMS and a members search function. All work done in React, TypeScript, Mocha and Test Cafe.",
+  },
+
+  {
+    index: "3",
+    workplace: "Booking.com",
+    imgMobile: bookingMobile,
+    imgDesktop: bookingDesktop,
+    summary:
+      "Worked within a team responsible for the North American rental car market at Booking.com/Rental Cars. I was hired to work on rewriting the old Java codebase into a sleeker React application. Predominantly worked with React and Jest.",
+  },
+
+  {
+    index: "4",
+    workplace: "Degree 53",
+    imgMobile: degreeMobile,
+    imgDesktop: degreeDesktop,
+    summary:
+      "Worked within the team responsible for building and maintaining the companies IP of white label gambling software. Technologies worked with were React, TypeScript and Jest.",
+  },
+
+  {
+    index: "5",
+    workplace: "Manchester Codes",
+    imgMobile: codesMobile,
+    imgDesktop: codesDesktop,
+    summary:
+      "Teacher at Manchester's only part time, evening coding boot camp. During the evenings (and some weekends) two nights a week, we teach learners of all ages how to code, from beginners who are looking to transition into a new career, to people who are looking to upskill in their current workplace. Curriculum includes JavaScript fundamentals, TDD, OOP, Node, Express.js, MongoDB, React and agile methodologies.",
+  },
+
+  {
+    index: "6",
+    workplace: "Super Kevin Keegan",
+    imgMobile: keggyMobile,
+    imgDesktop: keggyDesktop,
+    summary:
+      "The King Kev Quote Generator idea was manifested one day while searching for a certain quote from everybody's favourite 1970's footballer, Kevin Keegan. King Kev has a habit of saying some things that don't make sense to anyone but himself, he also happens to be incredibly unlucky, often when he's minding his own business or when he tries to do something good for someone else. Upon unsuccessfully finding a collated collection of Keggy's antics, the King Kev Quote Generator was born! It returns 50+ quotes/incidents from the great man himself at random. The website received International coverage when mentioned on a footballing podcast.",
+  },
+];
